8d45079fddc32c01953f78c15fd5ca469eedd290
Bradley Erickson <bbwe24@gmail.com>
2019-09-18 20:14:22 -0500

    updating router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import WelcomeScreen from './welcome.js';
 import MainMenu from './main-menu.js';
 import './App.css';
@@ -26,12 +26,12 @@ class App extends Component {
         const routes = ['background', 'example', 'practice', 'end'];
         return (
             <div className="default">
-                <HashRouter basename='/'>
+                <BrowserRouter basename='/'>
                     <Route exact path="/" component={() => <WelcomeScreen onStart={this.onStart} />} />
                     <Route exact path="/menu" component={() => <MainMenu name={name} />} />
                     <Route exact path="/addition/:type" component={() => <AdditionModule data={routes} name={name} />} />
                     <Route exact path="/subtraction/:type" component={() => <SubtractionModule data={routes} name={name} />} />
-                </HashRouter>
+                </BrowserRouter>
             </div>
         );
     }
