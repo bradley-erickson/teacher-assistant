bcf7bcc6e57ce85a8ddfa7defd5be7cdcd7a7b06
bgjones15 <bgjones15@winona.edu>
2019-11-12 18:32:33 -0600

    Created better getters and setters, modified to make async function calls

--- src/models/message.js
+++ src/models/message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    Author:{ type:String, required:true, trim: true, minlength:1 },
+    author:{ type:String, required:true, trim: true, minlength:1 },
     classID:{ type:Number, required:true, trim: true, minlength:1 },
     text:{ type:String, required:true, minlength:1 }
 });
--- src/models/student.js
+++ src/models/student.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
+    classID:{ type:Number, required:true, trim: true, minlength:1 },
     fname:{ type:String, required:true, trim: true, minlength:1 },
     lname:{ type:String, required:true, trim: true, minlength:1 },
     username:{ type:String, required:true, trim: true, minlength:1 },
