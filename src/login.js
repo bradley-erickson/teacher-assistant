c1d17d79654fc43e88c4aa676fd8c35b5d513b16
Bradley Erickson <bbwe24@gmail.com>
2019-12-01 14:09:09 -0600

    small fixes

--- src/login.js
+++ src/login.js
@@ -33,25 +33,19 @@ class Login extends Component {
     render() {
         const { pushTo, redirect } = this.state;
         return (
+            <div id="login" class="wrapper fadeInDown">
+                <div id="formContent">
+                    <i class="fa fa-graduation-cap fadeIn first" id="icon" alt="User Icon"/>
+                    <form id="userForm" >
+                        <input type="text" className="fadeIn second" placeholder="Name" onBlur="this.placeholder = 'Manager Id'" onfocus="this.placeholder = ''" ref={this.usernameRef} onChange={e => this.setState({ username: e.target.value })} />
+                        <input type="password" className="fadeIn third" name="password" placeholder="password" onBlur="this.placeholder = 'password'" onfocus="this.placeholder = ''" ref={this.passwordRef} onChange={e => this.setState({ password: e.target.value })} />
+                        <Button id="loginBtn" className="fadeIn fourth" onClick={this.login}>
+                                Begin
+                        </Button>
+                        {redirect && <Redirect to={pushTo} />}
+                    </form>
 
-                <div id="login" class="wrapper fadeInDown">
-                    <div id="formContent">
-
-                        <div class="fadeIn first">
-                        <i class="fa fa-graduation-cap" id="icon" alt="User Icon" />
-                        </div>
-
-
-            <form id="userForm" >
-              <input type="text" className="fadeIn second" placeholder="Name" onBlur="this.placeholder = 'Manager Id'" onfocus="this.placeholder = ''" ref={this.usernameRef} onChange={e => this.setState({ username: e.target.value })} />
-              <input type="password" className="fadeIn third" name="password" placeholder="password" onBlur="this.placeholder = 'password'" onfocus="this.placeholder = ''" ref={this.passwordRef} onChange={e => this.setState({ password: e.target.value })} />
-              <Button id="loginBtn" className="fadeIn fourth" onClick={this.login}>
-                    Begin
-                </Button>
-                {redirect && <Redirect to={pushTo} />}
-            </form>
-
-            </div>
+                </div>
             </div>
         );
     }
